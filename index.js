7588558cac9654feefa083036ca65193012dbb73
Dekevion Wade <Dekevionwade@yahoo.com>
2019-08-16 12:59:47 -0500

    idk

---
+++ index.js
@@ -0,0 +1,34 @@
+// start of program in main function
+//class above function
+class Books {
+    constructor(name, rating,author) {
+        this.name = name;
+        this.rating = rating;
+        this.author = author;
+    }
+
+
+}
+
+
+
+
+let tempBook1 = new Books("A",'B',"C");
+let tempbook2 = new Books("S",'A','M');
+let tempbook3 = new Books("1",'2','3');
+
+
+
+function main() {
+
+let arrayOfBooks = ["Book1","Book2","Book3"];
+let emptyBooks = [];
+
+ for(i=0; i<arrayOfBooks.length; i++){
+     console.log(emptyBooks.push(arrayOfBooks.push[i]));
+ }
+
+
+}
+
+main();
